838ce99e19de058775de7d6f7667a61d2358304b
BhadriIdeauxTech <bhadra@ideauxtech.com>
2025-04-07 14:03:12 +0530

    wave js

--- assets/js/wave.js
+++ assets/js/wave.js
@@ -241,14 +241,27 @@ void main() {
 
 gl_FragColor = v_color * texture2D(u_texture, gl_PointCoord);
 
-}`,onResize(w,h,dpi){const position=[],color=[]
-const width=400*(w/h)
-const depth=400
-const height=3
-const distance=5
-for(let x=0;x<width;x+=distance){for(let z=0;z<depth;z+=distance){position.push(-width/2+x,-30,-depth/2+z)
-color.push(0,1-(x/width)*1,0.5+x/width*0.5,z/depth)}}
-this.uniforms.field=[width,height,depth]
-this.buffers.position=position
-this.buffers.color=color
-this.uniforms.size=(h/400)*pointSize*dpi},})
+}`,onResize(w, h, dpi) {
+  const position = [];
+  const color = [];
+  let width = 400 * (w / h);
+  const depth = 400;
+  const height = 3;
+  const distance = 5;
+
+  // Ensure width is at least a small positive value
+  if (width <= 0) {
+    width = 1; // Or some other small positive default
+  }
+
+  for (let x = 0; x < width; x += distance) {
+    for (let z = 0; z < depth; z += distance) {
+      position.push(-width / 2 + x, -30, -depth / 2 + z);
+      color.push(0, 1 - (x / width) * 1, 0.5 + x / width * 0.5, z / depth);
+    }
+  }
+  this.uniforms.field = [width, height, depth];
+  this.buffers.position = position;
+  this.buffers.color = color;
+  this.uniforms.size = (h / 400) * pointSize * dpi;
+},})
